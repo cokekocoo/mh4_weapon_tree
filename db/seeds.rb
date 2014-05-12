e147abe670b5717fa67fcfe92746da1d90484db6
Ken Ohata <ohata@daraf.co.jp>
2014-05-12 22:11:47 +0900

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+yaml = YAML.load_file(Rails.root + "lib/twin_swords.yml")
+
+yaml.each do |key, value|
+  sword = TwinSword.new(name: key)
+  sword.assign_attributes(value)
+  sword.save!
+end
