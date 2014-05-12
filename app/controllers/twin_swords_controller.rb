1e21831682f6630f7e00533a529120e7f6e71a19
Ken Ohata <ohata@daraf.co.jp>
2014-05-12 23:06:09 +0900

    update twin_swords

--- app/controllers/twin_swords_controller.rb
+++ app/controllers/twin_swords_controller.rb
@@ -1,2 +1,23 @@
 class TwinSwordsController < ApplicationController
+  before_action :set_twin_swords
+
+  def index
+    render json: @twin_swords.map{ |sword| sword.to_hash }
+  end
+
+  def show
+    twin_sword = @twin_swords[params[:id]]
+
+    if twin_sword
+      render json: twin_sword
+    else
+      head :bad_request
+    end
+  end
+
+  private
+
+  def set_twin_swords
+    @twin_swords = YAMLTwinSword.new
+  end
 end
