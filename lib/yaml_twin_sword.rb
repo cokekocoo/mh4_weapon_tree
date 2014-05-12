75906217a1c6353472f64ce381d187085a1c797e
Ken Ohata <ohata@daraf.co.jp>
2014-05-12 22:55:05 +0900

    update YAMLTwinSword

--- lib/yaml_twin_sword.rb
+++ lib/yaml_twin_sword.rb
@@ -1,9 +1,29 @@
 class YAMLTwinSword
-  @@yaml = YAML.load_file(Rails.root + "lib/yaml/twin_swords.yml")
+  include Enumerable
 
-  class << self
-    def [] key
-      @@yaml[key]
+  def each
+    @@weapons.each{ |weapon|  yield weapon }
+  end
+
+  def [] key
+    @@weapons.find { |weapon| weapon.name == key }
+  end
+
+  class TwinSword
+    attr_reader :name
+
+    def initialize name, value
+      @name = name
+      @options = value
+      @weapons = ::YAMLTwinSword.new
     end
+
+    def derived_from
+      @weapons[@options["from"]]
+    end
+  end
+
+  @@weapons = YAML.load_file(Rails.root + "lib/yaml/twin_swords.yml").map do |key, value|
+    TwinSword.new key, value
   end
 end
