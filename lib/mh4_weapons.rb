52ee4d54eae765b646b17d6d8d3de6bb7781dfd4
Ken Ohata <ohata@daraf.co.jp>
2014-05-13 20:45:13 +0900

    update MH4Weapons

--- lib/mh4_weapons.rb
+++ lib/mh4_weapons.rb
@@ -1,6 +1,10 @@
 module MH4Weapons
+  def self.yaml_basepath
+    Rails.root + "lib/yaml/"
+  end
+
   def self.twin_swords
-    @@twin_swords ||= YAML.load_file(Rails.root + "lib/yaml/twin_swords.yml").map do |key, value|
+    @@twin_swords ||= YAML.load_file("#{self.yaml_basepath}twin_swords.yml").map do |key, value|
       TwinSword.new key, value
     end
   end
