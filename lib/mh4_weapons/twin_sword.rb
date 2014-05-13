4b2e0f2428137c9bc5b557d850b8bc4db41cfdb6
Ken Ohata <ohata@daraf.co.jp>
2014-05-13 19:36:03 +0900

    move

---
+++ lib/mh4_weapons/twin_sword.rb
@@ -0,0 +1,51 @@
+class MH4Weapons::TwinSword
+  attr_reader :name
+
+  def initialize name, value
+    @name = name
+    @options = value
+  end
+
+  def red_length
+    @options["red_length"]
+  end
+
+  def sharpness_length color
+    @options["#{color}_length"]
+  end
+
+  def sharpness_percentage color
+    sharpness_length(color).to_f / 72.to_f * 100
+  end
+
+  def derived_from
+    MH4Weapons.twin_swords.find{ |weapon| weapon.name == @options["from"] }
+  end
+
+  def deriving_to
+    MH4Weapons.twin_swords.select{ |weapon| weapon.derived_from == self }
+  end
+
+  def ancestors
+    if derived_from
+      [self] + derived_from.ancestors
+    else
+      [self]
+    end
+  end
+
+  def descendants
+    if deriving_to.any?
+      [self] + deriving_to.map(&:descendants).flatten
+    else
+      [self]
+    end
+  end
+
+  def to_hash
+    {
+      name: name,
+      from: derived_from
+    }
+  end
+end
