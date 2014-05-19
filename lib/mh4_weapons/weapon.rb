e2cb8974b9694fc6776ba859fd52724a7a3e95b7
Ken Ohata <ohata@daraf.co.jp>
2014-05-19 16:18:24 +0900

    define Weapon#recipes_deriving_to, recipe_derived_from

--- lib/mh4_weapons/weapon.rb
+++ lib/mh4_weapons/weapon.rb
@@ -1,14 +1,23 @@
 class MH4Weapons::Weapon < MH4Weapons::Base
-  def deriving_to
+
+  def recipes_deriving_to
     Recipe.all.select do |recipe|
       recipe.from == self
-    end.map(&:to)
+    end
+  end
+
+  def recipe_derived_from
+    Recipe.all.find do |recipe|
+      recipe.to == self
+    end
+  end
+
+  def deriving_to
+    recipes_deriving_to.map(&:to)
   end
 
   def derived_from
-    Recipe.all.find do |recipe|
-      recipe.to == self
-    end.from
+    recipe_derived_from.from
   end
 
   def ancestors
