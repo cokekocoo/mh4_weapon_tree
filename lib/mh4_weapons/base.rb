3b184af52c3f75696676cc767b1b136e2d507511
Ken Ohata <ohata@daraf.co.jp>
2014-05-19 15:53:20 +0900

    update Base

--- lib/mh4_weapons/base.rb
+++ lib/mh4_weapons/base.rb
@@ -1,24 +1,16 @@
 class MH4Weapons::Base
+  include MH4Weapons
+
   def self.yaml_basepath
     Rails.root + "lib/yaml/"
-  end
-
-  def self.filepath
-    @@filepath
   end
 
   def self.filepath= path
     @@filepath = path
   end
 
-  def self.map_yaml filename
-    YAML.load_file(self.yaml_basepath + filename).map do |key, value|
-      yield key, value
-    end
-  end
-
   def self.all
-    @@all ||= self.map_yaml self.filepath do |key, value|
+    @all ||= YAML.load_file(self.yaml_basepath + @@filepath).map do |key, value|
       self.new key, value
     end
   end
