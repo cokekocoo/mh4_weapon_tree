2ab2763843a32b7a8c0ca7b3945b0ff3875648bc
Ken Ohata <ohata@daraf.co.jp>
2014-05-19 16:18:38 +0900

    fix TwinSwordRecipe

--- lib/mh4_weapons/twin_sword_recipe.rb
+++ lib/mh4_weapons/twin_sword_recipe.rb
@@ -6,6 +6,6 @@ class MH4Weapons::TwinSwordRecipe < MH4Weapons::Base
   def initialize key, value
     @to    = MH4Weapons.all.find{ |w| w.name == key }
     @from  = MH4Weapons.all.find{ |w| w.name == value["from"] }
-    @items = Requirement.parse hash["materials"]
+    @items = Requirement.parse value["materials"]
   end
 end
